85a7c112cce7ab3f10fe25a305861acb26fee11c
schioler <bitmovers@gmail.com>
2013-08-24 09:59:17 +0200

    added select line

--- expense-dateline/src/main/sql/select-account-to-pattern.sql
+++ expense-dateline/src/main/sql/select-account-to-pattern.sql
@@ -1,1 +1,3 @@
-   select A.ID,A.NAME,P.ID,P.PATTERN from ACCOUNT A,PATTERN_MATCH P where A.id = P.account_id;
+   select A.ID as a_id,A.NAME,P.ID as p_id,P.PATTERN from ACCOUNT A,PATTERN_MATCH P where A.id = P.account_id;
+   
+   select * from LINE;
